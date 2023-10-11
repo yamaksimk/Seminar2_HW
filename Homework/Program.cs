375198e6e12bd6ea947b539af2010f22a89e8d35
yamaksimk <mkurakin2001@gmail.com>
2023-10-11 18:58:33 +0300

    Выполнил дз

---
+++ Homework/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+
+// Console.Write("Введите трехзначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n2 = n / 10 % 10;
+
+// if (n < 100 && n > 999)
+//     Console.WriteLine("Это не трехзначное число");
+// else
+//         Console.WriteLine($"Вторая цифра - {n2}");
+
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n3 = 0;
+// if (n < 100)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// if (n > 100 && n < 1000)
+// {
+//     n3 = n % 10;
+//     Console.WriteLine(n3);
+// }
+// if (n > 999)
+// {
+//     while (n > 999)
+//     {
+//         n = n / 10;
+//         n3 = n % 10;
+//         Console.WriteLine(n3);
+//     }
+// }
+
+
+// Console.Write("Введите день недели: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n > 0 && n < 6)
+//     Console.WriteLine("Это не выходной");
+// if (n > 5 && n < 8)
+//     Console.WriteLine("Это выходной");
+// if (n > 7)
+//     Console.WriteLine("Это не день недели");
